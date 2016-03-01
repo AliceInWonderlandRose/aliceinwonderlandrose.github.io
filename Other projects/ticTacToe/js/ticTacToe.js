840275331a449b80533b42a8ccaff9668e29b9b6
AliceInWonderlandRose <marieclare.trotta.95@gmail.com>
2016-02-29 19:04:25 -0500

    Updates Pictures

--- Other projects/ticTacToe/js/ticTacToe.js
+++ Other projects/ticTacToe/js/ticTacToe.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var circleOrEx = "o"; // var is the variable, circleOrEx is the class, "o" targets the o's.
   var isGameInProgress = true; // this code targets the the game while playing, not to erase the "x's" and "o's"
   var winningCombos = { // targets the winning game, either "x" or "o"
-    0: [ //0 is key
+    0: [ //0 is key, it strats off as zero becuase there is nothing there to start with.
       [1, 2], //this multiDimensional Array is values
       [3, 6],
       [4, 8]
@@ -50,10 +50,10 @@ $(document).ready(function() {
   // When you click the board during the game to make either an X or an O appear.
   $("#board").find("div").on("click", function() {
 
-    if (isGameInProgress && $(this).hasClass("empty")) { /// This is the start of the game. The grid is empty at the start of the game. Who ever gows first is either an "x" or an "o" when they click the first empty square. 
+    if (isGameInProgress && $(this).hasClass("empty")) { /// Validating that the game is in progress
       $(this).removeClass("empty").append("<span class='" + circleOrEx + "'>" + circleOrEx + "</span");
 
-      checkIfWon($(this).index(), circleOrEx); //This code, I believe is who ever wins the game ("x" or "o") the pop up will appear and state who was teh winner of the game. 
+      checkIfWon($(this).index(), circleOrEx); //This code, I believe is who ever wins the game ("x" or "o") the pop up will appear and state who was thw winner of the game. 
 
       if (circleOrEx === "o") {
         circleOrEx = "x";
@@ -64,29 +64,29 @@ $(document).ready(function() {
 
   });
 
-  // When you click the botton, a new grid (or game) will appear. 
+  // When you click the button, a new grid (or game) will appear. 
   $("#newGame").on("click", function() {
 
     var boardSquares = $("#board").find("div"); //locates the div in the HTML to buld a new grid. 
-    var firstEmptyMemorySquare = $(".container").find(".nine").filter(function() { //bonus Explain what filter does
+    var firstEmptyMemorySquare = $(".container").find(".nine").filter(function() { // filter is finding if the criteria for it to work is met
       return $.trim($(this).text()) === "" && $(this).children().length === 0;
     }).not("#board").first();
 
-    if (firstEmptyMemorySquare.length == 1) { //what is this if statement doing?
+    if (firstEmptyMemorySquare.length == 1) { // if firstEmptyMemorySquare is equal to 1 then it proceeds
       firstEmptyMemorySquare.html($("#board").html());
     } else {
       $(".container").find(".nine").not("#board").empty();
       $(".container").find(".nine").first().html($("#board").html());
     }
 
-    //Explain this each function
-    boardSquares.each(function() {
-      $(this).addClass("empty").empty();
+    //Once the game is finished, and the button is clicked, the div in the HTML will make a new board. 
+    boardSquares.each(function() { // the grid 
+      $(this).addClass("empty").empty();// the grid should be empty
     })
     isGameInProgress = true;
   })
 
-  //Explain this funciton, describe the parameters; what are the possible values of the paramaters
+  //This function is checking if player has won by looping through winning combos and if it's on a chosen square
   function checkIfWon(chosenSquare) {
 
     var mulitArr = winningCombos[chosenSquare];
@@ -100,13 +100,13 @@ $(document).ready(function() {
         }
       }
 
-      if (playerWon) { //Explain the condition and every line in the block
+      if (playerWon) { // This block of code deals with 
 
-        for (var j = 0; j < mulitArr[i].length; j++) {
-          $("#board").find("div").eq(mulitArr[i][j]).find("." + circleOrEx).addClass("green"); //Explain this condition
+        for (var j = 0; j < mulitArr[i].length; j++) { // tragetting the varable. 
+          $("#board").find("div").eq(mulitArr[i][j]).find("." + circleOrEx).addClass("green"); //To whichever person (the "x" or the "o") wins, the winner's line turns green
         }
-        $("#board").find("div").eq(chosenSquare).find("." + circleOrEx).addClass("green");
-        alert("Winner is " + circleOrEx.toUpperCase() + "!");
+        $("#board").find("div").eq(chosenSquare).find("." + circleOrEx).addClass("green"); // this chooses the square with the winning letter and turning it green
+        alert("Winner is " + circleOrEx.toUpperCase() + "!"); //alert pop up for whichever letter is the winner
         isGameInProgress = false;
         return false; //this exits the loop
       }
@@ -114,4 +114,4 @@ $(document).ready(function() {
 
 
   }
-})
+});
