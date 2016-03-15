19e625baecd4df5ce57570fe812593e285c63f20
AliceInWonderlandRose <marieclare.trotta.95@gmail.com>
2016-03-15 11:38:23 -0400

    fixed an error

--- Other projects/ajaxExamples/js/clients.js
+++ Other projects/ajaxExamples/js/clients.js
@@ -1,10 +1,10 @@
 
 	$("#getClients").on("click", function(){
 		//$.getJSON("http://http://AliceInWonderlandRose.github.io/Other%20projects/ajaxExamples/jasonDatabase/clients.json")
-	var url = "http://http://AliceInWonderlandRose.github.io/Other%20projects/ajaxExamples/jasonDatabase/clients.json"
+	var url = "http://AliceInWonderlandRose.github.io/Other%20projects/ajaxExamples/jasonDatabase/clients.json"
 	$.getJSON(url, function (data){
 	alert(data);
 	console.dir(data);
-	});
+	});// get JSON
 	
-	});
+	});//click
