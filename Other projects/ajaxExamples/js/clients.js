716c0614efa209867468db7715f694685c443341
AliceInWonderlandRose <marieclare.trotta.95@gmail.com>
2016-03-15 11:43:37 -0400

    Js

--- Other projects/ajaxExamples/js/clients.js
+++ Other projects/ajaxExamples/js/clients.js
@@ -1,10 +1,16 @@
-
+$(document).ready(function(){
 	$("#getClients").on("click", function(){
 		//$.getJSON("http://http://AliceInWonderlandRose.github.io/Other%20projects/ajaxExamples/jasonDatabase/clients.json")
-	var url = "http://AliceInWonderlandRose.github.io/Other%20projects/ajaxExamples/jasonDatabase/clients.json"
+	var url = "http://AliceInWonderlandRose.github.io/Other%20projects/ajaxExamples/jasonDatabase/clients.json";
 	$.getJSON(url, function (data){
-	alert(data);
-	console.dir(data);
+	$.each(data, function(index, item){//item = each object
+		$("#data").append(item.name);
+		});
+	
+	//alert(data);
+	//console.dir(data);
 	});// get JSON
 	
 	});//click
+	
+});
