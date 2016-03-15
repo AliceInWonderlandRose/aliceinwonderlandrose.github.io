d759da45da98668d2f935b57c773af28c94dbc5d
AliceInWonderlandRose <marieclare.trotta.95@gmail.com>
2016-03-15 12:09:43 -0400

    added a table

--- Other projects/ajaxExamples/js/clients.js
+++ Other projects/ajaxExamples/js/clients.js
@@ -1,12 +1,21 @@
-$(document).ready(function(){
+ $(document).ready(function(){
 	$("#getClients").on("click", function(){
 		//$.getJSON("http://http://AliceInWonderlandRose.github.io/Other%20projects/ajaxExamples/jasonDatabase/clients.json")
 	var url = "http://AliceInWonderlandRose.github.io/Other%20projects/ajaxExamples/jasonDatabase/clients.json";
 	$.getJSON(url, function (data){
+var html = "<table>" +
+"<tr><th>Name</th><th>Email</th><th>Company</th></tr>";	
+	
 	$.each(data, function(index, item){//item = each object
-		$("#data").append(item.name);
+		html+="<tr>"+
+		"<td>" + item.name+ "</td>" +
+		"<td>" + item.email+ "</td>" +
+		"<td>" + item.company+ "</td>" +
+		"</tr>";
 		});
 	
+	html =+ "</table>";
+	$("#data").append(html);
 	//alert(data);
 	//console.dir(data);
 	});// get JSON
