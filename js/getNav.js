4025b0d8e2b7a98963bb45b499400b24be6b4dd5
AliceInWonderlandRose <marieclare.trotta.95@gmail.com>
2016-03-23 15:09:25 -0400

    footer

--- js/getNav.js
+++ js/getNav.js
@@ -7,6 +7,14 @@ $.get("http://AliceInWonderlandRose.github.io/partials/nav.html", function(data)
 	//Place code for the footer in here
 	});
 
+	});
+$.get("http://AliceInWonderlandRose.github.io/partials/footer.html", function(data){
 	
+	$(document).ready(function(){
 	
+	$(".container").append(data);
+	$(".container").fadeIn();
+	//Place code for the footer in here
+	});
+
 	});
