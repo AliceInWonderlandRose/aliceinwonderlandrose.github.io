4592a1d085275955a38e01fa74984cdb1ebd5646
AliceInWonderlandRose <marieclare.trotta.95@gmail.com>
2016-03-29 10:15:33 -0400

    Final Website

--- Other projects/Final Project/js/final.js
+++ Other projects/Final Project/js/final.js
@@ -1,10 +1,33 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+$(document).ready(function(){
+	
+	// Get all the nav li, add click event
+	$(".nav").find("li").on("click",function(){
+		// remove all active class
+		$(".nav").find("li").removeClass("active");
+		// add active class to clicked it. 
+		$(this).addClass("active");
+		
+		var page = $(this).attr("id");
+		getPartial(page);
+	
+	});// closes the $(".nav").find("li").on("click",function()
+	
+	function getPartial(partial) {
+		if (partial === "homePage"){
+				$.get("partials/home.html", function(data){
+					$("#pageContent").html(data);
+					$('.carousel').carousel();
+		});
+		
+		}else if (partial === "rentPage"){
+		
+		
+		}else if (partial === "orderPage"){
+			
+			
+			}
+	}
+	// begin Program, get the homepage
+	getPartial("homePage");
+	
+}); //closes document ready. 
