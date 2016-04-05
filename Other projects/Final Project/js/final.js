a2a7c65e54e18bb91fb4f437edfb1d852db222d8
AliceInWonderlandRose <marieclare.trotta.95@gmail.com>
2016-04-05 09:18:47 -0400

    new files

--- Other projects/Final Project/js/final.js
+++ Other projects/Final Project/js/final.js
@@ -18,10 +18,11 @@ $(document).ready(function(){
 				$.get("partials/home.html", function(data){
 					$("#pageContent").html(data);
 					$(".carousel").carousel();
-					 $('#gallery').blindify({ //Styling for My Carousel
-        color : '#FE7F6C',
-        numberOfBlinds : 40,
-        slideVisibleTime : 5000
+					 
+					 $('#myCarousel').blindify({ //Styling for My Carousel
+        			color : '#FE7F6C',
+        			numberOfBlinds : 40,
+       				 slideVisibleTime : 5000
     });
     
     // start and configure the Morphext plugin
