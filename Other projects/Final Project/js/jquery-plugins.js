998c44094452457614b4ec3d62faa1b417bc6d46
AliceInWonderlandRose <marieclare.trotta.95@gmail.com>
2016-04-05 12:42:26 -0400

    sjdfbkajsdh

--- Other projects/Final Project/js/jquery-plugins.js
+++ Other projects/Final Project/js/jquery-plugins.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    console.log( 'jQuery started.' );
-    
+  
     // start and configure the blindify plugin
     $('#gallery').blindify({
         color : '#FE7F6C',
