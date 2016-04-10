d4559263c99755036cc480a9cef602c33f203e19
AliceInWonderlandRose <marieclare.trotta.95@gmail.com>
2016-04-09 21:06:52 -0400

    comment

--- Other projects/Final Project/js/final.js
+++ Other projects/Final Project/js/final.js
@@ -67,7 +67,7 @@ $(document).ready(function() {
 
                         $("#pageContent").html(data);
 
-                        $('#startRentDate, #endRentDate').datepicker({});
+                       // $('#startRentDate, #endRentDate').datepicker({});
 	// jQurey events in here
 					// First jQurey Event
 						$("#startRentDate").on("focus", function(){
