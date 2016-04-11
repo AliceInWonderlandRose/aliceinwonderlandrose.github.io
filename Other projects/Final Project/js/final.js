4e9e40c904defb25cc15958d65d242e83f9be364
AliceInWonderlandRose <marieclare.trotta.95@gmail.com>
2016-04-11 19:37:46 -0400

    new image checkmark

--- Other projects/Final Project/js/final.js
+++ Other projects/Final Project/js/final.js
@@ -175,7 +175,7 @@ $(document).ready(function() {
             $("#successMsg").html("Order Received!<br/><br/>" +
               order.catSelect + " will be delivered on " + 
               order.startRentDate +
-              "<img id='paws' src='images/catPaws.jpeg'>");
+              "<img id='checkmark' src='images/checkmark.png'>");
 
         } //sendConfirmation
 
